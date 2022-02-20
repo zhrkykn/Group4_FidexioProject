546f7a2d5d649f5ba34e428b91877bb301036902
ZerNurgul <zernurgul@gmail.com>
2022-02-20 16:34:16 +0000

    added button locators

---
+++ src/test/java/com/fidexio/step_defs/inventorySteps.java
@@ -0,0 +1,44 @@
+package com.fidexio.step_defs;
+
+import com.fidexio.pages.InventoryPage;
+import com.fidexio.pages.LoginPage;
+import com.fidexio.utilities.BrowserUtils;
+import com.fidexio.utilities.ConfigurationReader;
+import com.fidexio.utilities.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
+
+public class inventorySteps {
+     InventoryPage invent=new InventoryPage();
+    LoginPage loginPage=new LoginPage();
+    @Given("the user is on the login in")
+    public void the_user_is_on_the_login_in() {
+        String url = ConfigurationReader.get("url");
+        Driver.get().get(url);
+        loginPage.validLogin();
+
+        BrowserUtils.waitFor(2);
+    }
+
+    @Given("the user is clicked inventory button")
+    public void the_user_is_clicked_inventory_button() {
+    invent.inventory.click();
+
+    }
+
+    @Given("the user clicked the product button")
+    public void the_user_clicked_the_product_button() {
+invent.product.click();
+    }
+
+    @Then("the create button seen")
+    public void the_create_button_seen() {
+        BrowserUtils.waitFor(3);
+        String createbtn = "Create";
+        Assert.assertEquals("verify home page",invent.create.getText(),createbtn);
+
+    }
+
+
+}
