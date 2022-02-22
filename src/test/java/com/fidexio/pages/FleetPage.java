d2061e1cae4698c737b5924907f4ad4a8a8eb8f6
zhrkykn <zehrakoyukan@gmail.com>
2022-02-22 18:04:11 +0300

    Fleet Vehicle Odometer is completed

--- src/test/java/com/fidexio/pages/FleetPage.java
+++ src/test/java/com/fidexio/pages/FleetPage.java
@@ -18,6 +18,32 @@ public class FleetPage extends BasePage{
     @FindBy(xpath = "//div[@data-menu-parent='134']//li/a")
     public List<WebElement> fleetSideMenuOptions;
 
+    @FindBy(xpath = "//span[contains(text(),'Vehicles Odometer')]")
+    public WebElement vehiclesOdoButton;
+
+    @FindBy(xpath = "//button[@accesskey='c']")
+    public WebElement odoCreateButton;
+
+    @FindBy(css = ".o_input_dropdown")
+    public WebElement vehicleOdoName;
+
+    @FindBy(linkText ="Opel/Astra/1-ACK-205")
+    public WebElement vehicleOdoSelect;
+
+    @FindBy(css=".o_field_float.o_field_number.o_field_widget.o_input.oe_inline")
+    public WebElement odoValue;
+
+    @FindBy(css=".o_datepicker_input.o_input")
+    public WebElement odoDate;
+
+    @FindBy(xpath = "//button[@accesskey='s']")
+    public WebElement odoSaveButton;
+
+
+    @FindBy(css =".o_notification.undefined.o_error")
+    public WebElement odoError2;
+
+
     public void clickFleet(){
 
         try {
---
+++ src/test/resources/features/FleetVehicleOdo.feature
@@ -0,0 +1,19 @@
+Feature:Fleet Vehicle Odometer
+
+  Background:
+    Given The user should be logged in with valid credentials
+    When the user clicks to fleetPage
+    Then the user clicks to Vehicle Odometer
+    And Click on Create button
+
+  Scenario: User create Vehicles Odometer in fleet menu
+    When Choose the vehicle name
+    Then Enter odometer value
+    And Enter date
+    And Click on Save button
+
+  Scenario: User can not create Vehicles Odometer without vehicle name
+    Then Enter odometer value
+    And Enter date
+    And Click on Save button
+    And Error message displayed
