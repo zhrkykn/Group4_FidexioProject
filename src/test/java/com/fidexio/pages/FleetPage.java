6ed877c40ed837524ac327d3eb7a68b00927029e
Halid Mahmutyazıcıoğlu <hmyazicioglu_7@hotmail.com>
2022-02-25 15:09:09 +0300

    Vehicle Fuel Log has been done

--- src/test/java/com/fidexio/pages/FleetPage.java
+++ src/test/java/com/fidexio/pages/FleetPage.java
@@ -3,19 +3,63 @@ package com.fidexio.pages;
 import com.fidexio.utilities.BrowserUtils;
 
 
-
+import com.fidexio.utilities.Driver;
+import org.openqa.selenium.DeviceRotation;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.NoSuchElementException;
+import java.util.concurrent.TimeUnit;
 
 public class FleetPage extends BasePage{
     @FindBy(css = "#menu_more_container>a")
     public WebElement menuMore;
 
-
     @FindBy(xpath = "(//span[@class='oe_menu_text'])[21]")
     public WebElement fleet_Menu;
+    // --> halid
+    @FindBy(xpath = "//span[contains(text(),'Vehicles Fuel Logs')]")
+    public WebElement vehicleFuelLog;
+
+    @FindBy(xpath = "//button[@class='btn btn-sm btn-default o_button_import']")
+    public WebElement importButton;
+
+    @FindBy(css = "button.btn.btn-primary.btn-sm.o_list_button_add")
+    public WebElement createButton;
+
+    @FindBy(xpath = "//div[@class='o_input_dropdown']/input")
+    public WebElement dropdownVehicle;
+
+    @FindBy(xpath = "(//li[@class='ui-menu-item']/a)[2]")
+    public WebElement vehicle;
+
+    @FindBy(xpath = "//input[@name='liter']")
+    public WebElement liter;
+
+    @FindBy(xpath = "//input[@name='price_per_liter']")
+    public WebElement pricePerLiter;
+
+    @FindBy(xpath = "//input[@name='odometer']")
+    public WebElement odometer;
+
+    @FindBy(xpath = "//button[@accesskey='s']")
+    public WebElement saveButton;
+
+    @FindBy(xpath = "//div[@class='o_notification_manager']")
+    public WebElement warningMessage;
+
+    public void setCreateButton(){
+        Driver.get().manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+        createButton.click();
+    }
+
+    public void setImportButton(){
+        BrowserUtils.waitForVisibility(importButton,10);
+    }
+
+    //<-- halid
 
     public void clickFleet(){
 
@@ -39,5 +83,6 @@ public class FleetPage extends BasePage{
             BrowserUtils.waitForPageToLoad(5);
         }
     }
+
 
 }
---
+++ src/test/resources/features/fleet.feature
@@ -0,0 +1,31 @@
+
+  Feature: Fleet page
+
+    Background:
+      Given the user is on the login page
+      When Enter the user credentials
+
+
+    @wip1
+    Scenario:Create Vehicles Fuel Log
+      When the user clicks on fleet link
+      Then the user clicks on Vehicles Fuel Logs
+      And the user clicks on create button
+      Then the user clicks on vehicle dropdown
+      And the user chooses any car
+      Then the user fills out liters
+      And the user fills out price per liter
+      Then the user fills out odometer value
+      Then the user clicks on save button
+
+    @wip2
+    Scenario: Create Vehicles Fuel Log Without a Vehicle
+      When the user clicks on fleet link
+      Then the user clicks on Vehicles Fuel Logs
+      And the user clicks on create button
+      Then the user clicks on vehicle dropdown
+      Then the user fills out liters
+      And the user fills out price per liter
+      Then the user fills out odometer value
+      Then the user clicks on save button
+      And the user is able to see warning message
