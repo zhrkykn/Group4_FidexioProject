6a347f281fbf7ddefaf9b79967bbb508f66df26f
Nargiz <nartiss17@gmail.com>
2022-02-25 01:17:11 +0000

    Fleet Contract Fleet Step Defs created

---
+++ src/test/java/com/fidexio/step_defs/FleetContractStepDefs.java
@@ -0,0 +1,51 @@
+package com.fidexio.step_defs;
+
+import com.fidexio.pages.FleetPage;
+import com.fidexio.utilities.BrowserUtils;
+import com.fidexio.utilities.Driver;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
+public class FleetContractStepDefs {
+    FleetPage fleetPage=new FleetPage();
+
+    @Then("user navigate to Vehicles Contracts")
+    public void user_navigate_to_Vehicles_Contracts() {
+        fleetPage.vehicleContractMenu.click();
+        BrowserUtils.waitFor(5);
+
+
+    }
+    @When("user click to vehicle dropdown in vehicle contract menu")
+    public void user_click_to_vehicle_dropdown_in_vehicle_contract_menu() {
+       fleetPage.vehicleContractDropdown.click();
+        BrowserUtils.waitFor(5);
+
+    }
+
+
+
+    @When("user select Audi option in vehicle dropdown")
+    public void user_select_Audi_option_in_vehicle_dropdown() {
+        fleetPage.vehicleAudiOption.click();
+        BrowserUtils.waitFor(5);
+
+    }
+
+
+
+    @When("user click on Save Button")
+    public void userClickOnContractSaveButton() {
+        fleetPage.saveButton.click();
+            BrowserUtils.waitFor(5);
+
+
+
+        }
+    }
