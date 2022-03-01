0db3bf3ac7611ce75ca732d3a6d0749a1ff8d994
Nilay Keser Demir <nilaykdemir@gmail.com>
2022-03-01 21:47:10 +0000

    PointOfSalePage locators found

--- src/test/java/com/fidexio/pages/PointOfSalePage.java
+++ src/test/java/com/fidexio/pages/PointOfSalePage.java
@@ -1,6 +1,32 @@
 package com.fidexio.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class PointOfSalePage extends BasePage {
+
+    @FindBy(xpath = "//a[@data-menu=\"484\"]/span")
+    public WebElement PointOfSales;
+
+    @FindBy(xpath= "//a[@data-menu=\"496\"]/span")
+    public WebElement PointOfSalesLeft;
+
+    @FindBy(xpath = "//div[@class=\"o_list_buttons\"]/button[1]")
+    public WebElement CreateButton;
+
+    @FindBy(xpath ="//div[@class=\"o_form_buttons_edit\"]/button[1]")
+    public WebElement SaveButton;
+
+    @FindBy(xpath = "//div[@class=\"o_form_buttons_edit\"]/button[2]")
+    public WebElement DiscardButton;
+
+    @FindBy(xpath = "//div[@class=\"oe_title\"]//input")
+    public WebElement PointOfSaleName;
+
+    @FindBy(xpath= "//div[@class=\"btn-group btn-group-sm o_cp_switch_buttons\"]/button[2]")
+    public WebElement KanbanButton;
+
+
 
 
 
