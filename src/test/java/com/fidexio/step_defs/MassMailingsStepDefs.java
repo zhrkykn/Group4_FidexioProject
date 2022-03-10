836cfba3ef80f69f512e1d0f1c903306b9014240
FatmaCE <fcelik.pc@gmail.com>
2022-03-10 14:12:22 +0000

    MassMailings feature and stepDefs were edited

---
+++ src/test/java/com/fidexio/step_defs/MassMailingsStepDefs.java
@@ -0,0 +1,69 @@
+package com.fidexio.step_defs;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class MassMailingsStepDefs {
+
+    @When("user enters subject name as {string}")
+    public void user_enters_subject_name_as(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("verify that title changes to {string}")
+    public void verify_that_title_changes_to(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("user clicks on a random Mass mailings")
+    public void user_clicks_on_a_random_Mass_mailings() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user edits the name of the subject")
+    public void the_user_edits_the_name_of_the_subject() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("Verify that the Subject name has been changed")
+    public void verify_that_the_Subject_name_has_been_changed() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user clicks on a random Mass mailings")
+    public void the_user_clicks_on_a_random_Mass_mailings() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user clicks on the {string} from the pop up button")
+    public void the_user_clicks_on_the_from_the_pop_up_button(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("user verifies the Mass mailings number has not changed")
+    public void user_verifies_the_Mass_mailings_number_has_not_changed() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("the user opens Actions dropdown and clicks Delete")
+    public void the_user_opens_Actions_dropdown_and_clicks_Delete() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("verify that  Mass Mailings  has been deleted")
+    public void verify_that_Mass_Mailings_has_been_deleted() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+    }
--- src/test/resources/features/MassMailings.feature
+++ src/test/resources/features/MassMailings.feature
@@ -1,4 +1,4 @@
-Feature: Sales Manager should be able to create SALES / ORDERS
+Feature: Sales Manager should be able to create Email Marketings / Mass Mailings
 
   Background:
     Given sales manager should be logged in
@@ -10,13 +10,9 @@ Feature: Sales Manager should be able to create SALES / ORDERS
     And the user clicks on the "Save" button
     Then verify that title changes to "Third Newsletter - Odoo"
 
-  Scenario: Verify that Kanban button works as expected
-    When user clicks on the "Kanban" button
+  Scenario: Verify that Kanban button works as expected in the Mass Mailings Page
+    When the user clicks on the "Kanban" button
     Then verify that it switched to "Kanban" view
-
-  Scenario: Verify that List button works as expected
-    When user clicks on the "List" button
-    Then verify that it switched to "List" view
 
 
   Scenario: Verify that sales manager can edit mass mailings
@@ -27,7 +23,7 @@ Feature: Sales Manager should be able to create SALES / ORDERS
     Then Verify that the Subject name has been changed
 
   Scenario:Verify that sales manager can discard changes
-    When user clicks on a random Mass mailings
+    When the user clicks on a random Mass mailings
     And the user clicks on the "Edit" button
     And the user edits the name of the subject
     And the user clicks on the "Discard" button
@@ -35,7 +31,7 @@ Feature: Sales Manager should be able to create SALES / ORDERS
     Then user verifies the Mass mailings number has not changed
 
   Scenario: Verify that the user can delete Mass Mailings
-    When user clicks on a random Mass mailings
+    When the user clicks on a random Mass mailings
     And the user opens Actions dropdown and clicks Delete
     And the user clicks on the "Ok" from the pop up button
     Then verify that  Mass Mailings  has been deleted
